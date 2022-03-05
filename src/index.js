238db3d7460c29f7f8add516241b4fa94aa44d01
RdotSilva <RyandotSilva@gmail.com>
2022-03-05 10:46:09 +0000

    Fetch job cards

--- src/index.js
+++ src/index.js
@@ -18,5 +18,22 @@ async function scrape() {
 
   const totalResults = spanTexts[1];
   console.log(totalResults);
+
+  // Get individual job cards, should match the total search results above in length
+  let jobs = await page.evaluate(() => {
+    let data = [];
+    let elements = document.getElementsByClassName("job-cards-council");
+
+    console.log(`elements: ${elements}`);
+
+    for (var element of elements) {
+      data.push(element.getElementsByTagName("p"));
+    }
+    return data;
+  });
+
+  console.log(jobs);
+
+  browser.close();
 }
 scrape();
