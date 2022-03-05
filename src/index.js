556bd3e0b49471f4ab8b7eb7e1319428f2a0eeec
RdotSilva <RyandotSilva@gmail.com>
2022-03-05 10:23:28 +0000

    Fetch total results from search

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 const dotenv = require("dotenv");
+const puppeteer = require("puppeteer");
+
 dotenv.config();
 
 const { JOB_URL: jobUrl } = process.env;
+
+async function scrape() {
+  const browser = await puppeteer.launch({});
+  const page = await browser.newPage();
+
+  await page.goto(jobUrl);
+
+  // Total search results are kept in a span
+  const spanTexts = await page.$$eval("span", (elements) =>
+    elements.map((el) => el.innerText)
+  );
+
+  const totalResults = spanTexts[1];
+  console.log(totalResults);
+}
+scrape();
