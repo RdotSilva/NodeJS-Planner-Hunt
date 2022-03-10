200d0b10c29312274c805d9ee4b2011bc893a544
RdotSilva <RyandotSilva@gmail.com>
2022-03-10 18:47:26 +0000

    Add updateJobRecord function

--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,16 @@
 const Datastore = require("nedb");
+const path = require("path");
 
 const databasePath = path.join(__dirname, "../input/jobs.json");
 const dbConnection = new Datastore({ filename: databasePath, autoload: true });
 const dbReferenceId = "fJ0p8GfKYkEwgbSm";
+
+const updateJobRecord = (newResult) => {
+  dbConnection.update({ _id: dbReferenceId }, newResult, {}, function () {
+    console.log("Doc updated");
+  });
+};
+
+module.exports = {
+  updateJobRecord,
+};
