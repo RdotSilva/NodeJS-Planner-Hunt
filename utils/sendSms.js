d76f3f017d952bbb94ffdc1d64a39528c7fae9aa
RdotSilva <RyandotSilva@gmail.com>
2022-03-11 17:46:54 +0000

    Add JSDOc comments

--- utils/sendSms.js
+++ utils/sendSms.js
@@ -11,6 +11,11 @@ const {
 
 const twilioClient = new twilio(twilioSid, twilioToken);
 
+/**
+ * Format results so they are easily readable in text message
+ * @param {Array} results List of result links
+ * @returns
+ */
 const formatResults = (results) => {
   let links = "";
   results.forEach((result) => {
