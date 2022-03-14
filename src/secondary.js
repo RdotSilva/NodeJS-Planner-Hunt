866fcee86e23f463aad244109c07424ae76bb690
RdotSilva <RyandotSilva@gmail.com>
2022-03-14 09:56:09 -0400

    Add scrape function

--- src/secondary.js
+++ src/secondary.js
@@ -6,3 +6,16 @@ const colors = require("colors");
 const jobOutputPath = path.join(__dirname, "../output/secondary.txt");
 dotenv.config();
 const { SECONDARY_JOB_URL: jobUrl, SECONDARY_BASE_URL: baseUrl } = process.env;
+
+/**
+ * Main function to fetch secondary jobs
+ */
+async function scrape() {
+  const browser = await puppeteer.launch({});
+  const page = await browser.newPage();
+
+  await page.goto(jobUrl);
+
+  browser.close();
+}
+scrape();
