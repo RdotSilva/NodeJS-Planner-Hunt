28715cafeeae01e47dd48129770b0d2288c68953
RdotSilva <RyandotSilva@gmail.com>
2022-03-18 21:43:07 -0400

    Add test case for formatTotal

--- test/formatResults.test.js
+++ test/formatResults.test.js
@@ -2,6 +2,7 @@ const assert = require("assert");
 const {
   formatResults,
   formatResultsForFile,
+  formatTotal,
 } = require("../utils/formatResults");
 
 describe("FormatResults", function () {
@@ -22,6 +23,14 @@ describe("FormatResults", function () {
         formatted,
         `${newDate.toDateString()} - ${newDate.toLocaleTimeString()}: a,b,c`
       );
+    });
+  });
+
+  describe("#formatTotal()", function () {
+    it("should return a string that contains total number of results", function () {
+      const results = ["a", "b", "c"];
+      const formatted = formatTotal(results);
+      assert.equal(formatted, `Total results: 3`);
     });
   });
 });
