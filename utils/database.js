e72ae61f04d1c9fafd1ab6e3c84430442e1ac31e
RdotSilva <RyandotSilva@gmail.com>
2022-03-22 20:03:58 -0400

    Add JSDoc comments

--- utils/database.js
+++ utils/database.js
@@ -5,12 +5,19 @@ const databasePath = path.join(__dirname, "../input/jobs.json");
 const dbConnection = new Datastore({ filename: databasePath, autoload: true });
 const dbReferenceId = "fJ0p8GfKYkEwgbSm";
 
+/**
+ * Update a job record in the database
+ * @param {String} newResult New data to update the job record
+ */
 const updateJobRecord = (newResult) => {
   dbConnection.update({ _id: dbReferenceId }, newResult, {}, function () {
     console.log("Doc updated");
   });
 };
 
+/**
+ * Find a job record by ID
+ */
 const findJobRecord = new Promise((resolve, reject) => {
   dbConnection.findOne({ _id: dbReferenceId }, function (err, doc) {
     resolve(doc);
