7f9a9e0ad6d06a17288329c19c6e18e17c04c13b
RdotSilva <RyandotSilva@gmail.com>
2022-03-24 21:58:48 -0400

    Fix TODO for sending email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -7,35 +7,39 @@ const emailSentOutputPath = path.join(__dirname, "../output/emailSent.txt");
 dotenv.config();
 const { FROM_EMAIL: fromEmail, TO_EMAIL: toEmail } = process.env;
 
-// TODO: Create function to send email (this will be used rather than sending SMS)
+const sendEmail = () => {
+  let testAccount = await nodemailer.createTestAccount();
 
-let testAccount = await nodemailer.createTestAccount();
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
 
-// create reusable transporter object using the default SMTP transport
-let transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
-  port: 587,
-  secure: false, // true for 465, false for other ports
-  auth: {
-    user: testAccount.user, // generated ethereal user
-    pass: testAccount.pass, // generated ethereal password
-  },
-});
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: `Test User" ${fromEmail}`, // sender address
+    to: toEmail, // list of receivers
+    subject: "Test Email", // Subject line
+    text: "This is only a test", // plain text body
+    html: "<b>Test</b>", // html body
+  });
 
-// send mail with defined transport object
-let info = await transporter.sendMail({
-  from: `Test User" ${fromEmail}`, // sender address
-  to: toEmail, // list of receivers
-  subject: "Test Email", // Subject line
-  text: "This is only a test", // plain text body
-  html: "<b>Test</b>", // html body
-});
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-console.log("Message sent: %s", info.messageId);
-// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
-// Preview only available when sending through an Ethereal account
-console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-// Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  fs.appendFileSync(emailSentOutputPath, new Date());
+};
 
-fs.appendFileSync(emailSentOutputPath, new Date());
+module.exports = {
+  sendEmail,
+};
