a21eb07828b06449a7d8b5891215a0f55ea01bc7
RdotSilva <RyandotSilva@gmail.com>
2022-03-27 19:35:02 +0100

    Remove placeholder values for email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -7,7 +7,7 @@ const emailSentOutputPath = path.join(__dirname, "../output/emailSent.txt");
 dotenv.config();
 const { FROM_EMAIL: fromEmail, TO_EMAIL: toEmail } = process.env;
 
-const sendEmail = () => {
+const sendEmail = (results) => {
   let testAccount = await nodemailer.createTestAccount();
 
   // create reusable transporter object using the default SMTP transport
@@ -23,20 +23,15 @@ const sendEmail = () => {
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: `Test User" ${fromEmail}`, // sender address
-    to: toEmail, // list of receivers
-    subject: "Test Email", // Subject line
-    text: "This is only a test", // plain text body
-    html: "<b>Test</b>", // html body
+    from: `Test User" ${fromEmail}`,
+    to: toEmail,
+    subject: "New Results Found!",
+    text: results,
   });
 
-  console.log("Message sent: %s", info.messageId);
+  console.log(`Message sent: ${info.messageId}`);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-
+  
   fs.appendFileSync(emailSentOutputPath, new Date());
 };
 
