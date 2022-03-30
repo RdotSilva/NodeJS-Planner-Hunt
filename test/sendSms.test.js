270ebea30244b5b3e248dd43c690d447f67d56f7
RdotSilva <RyandotSilva@gmail.com>
2022-03-30 20:12:33 +0100

    Add test case for successful SMS

--- test/sendSms.test.js
+++ test/sendSms.test.js
@@ -1,1 +1,12 @@
-// TODO: Add tests for SMS (will have to mock)
+const assert = require("assert");
+const { sendSms } = require("../utils/sendSms");
+
+describe("SendSms", function () {
+  describe("#sendSms()", function () {
+    it("success should send SMS", function () {
+      const smsSent = await sendSms();
+      const expected = `SMS sent`
+      assert.equal(smsSent, expected);
+    });
+  });
+});
