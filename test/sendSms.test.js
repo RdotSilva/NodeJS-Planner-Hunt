953f99f8c242bc968cdcd2d0a8562dace7437648
RdotSilva <RyandotSilva@gmail.com>
2022-03-30 20:13:32 +0100

    Add test case for SMS failure

--- test/sendSms.test.js
+++ test/sendSms.test.js
@@ -4,9 +4,15 @@ const { sendSms } = require("../utils/sendSms");
 describe("SendSms", function () {
   describe("#sendSms()", function () {
     it("success should send SMS", function () {
-      const smsSent = await sendSms();
+      const smsSent = await sendSms('pass');
       const expected = `SMS sent`
       assert.equal(smsSent, expected);
     });
+      
+    it("failure should not send SMS", function () {
+        const smsSent = await sendSms('fail');
+        const expected = `Unable to send SMS`
+        assert.equal(smsSent, expected);
+      });
   });
 });
